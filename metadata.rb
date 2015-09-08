d264958b4cb6627b0f6afe6eb9c27d56edb7ca71
Andrew Repton <arepton@schubergphilis.com>
2015-09-08 11:57:29 +0200

    Newer version of golang and rubocop fixes

--- metadata.rb
+++ metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.0'
 
 depends 'ark', '~> 0.9.0'
-depends 'golang', '~> 1.4.0'
+depends 'golang', '~> 1.5.0'
--- recipes/install_binary.rb
+++ recipes/install_binary.rb
@@ -13,7 +13,7 @@ ark 'packer' do
   url "#{node['packer']['url_base']}/packer_#{node['packer']['version']}_#{node['os']}_#{node['packer']['arch']}.zip"
   version node['packer']['version']
   checksum node['packer']['checksum']
-  has_binaries ["packer"]
+  has_binaries ['packer']
   append_env_path false
   strip_components 0
 
