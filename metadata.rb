3adcb01557104111494f3ff400aa453821d42942
Dang H. Nguyen <haidangwa@gmail.com>
2016-02-25 11:19:30 -0800

    updated cookbook version, updated upstream ark and golang cookbook dependencies

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'arepton@schubergphilis.com'
 license 'Apache 2.0'
 description 'Installs/Configures packer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.1.2'
+version '1.2.0'
 
-depends 'ark', '~> 0.9.0'
-depends 'golang', '~> 1.5.0'
+depends 'ark', '~> 1.0.0'
+depends 'golang', '~> 1.7.0'
