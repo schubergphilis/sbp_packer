b69b4235204b580cbb144613c57ac45cd2d4b92d
Dang H. Nguyen <haidangwa@gmail.com>
2016-02-25 12:03:31 -0800

    fixed install_binary_test

--- files/default/tests/minitest/install_binary_test.rb
+++ files/default/tests/minitest/install_binary_test.rb
@@ -3,21 +3,24 @@ require_relative 'support/test_helper'
 describe_recipe 'sbp_packer::install_binary' do
   include ChefPacker::TestHelper
 
+  before(:each) do
+    @packer_dir = "/usr/local/packer-#{node['packer']['version']}"
+  end
+
   it 'installed packer' do
     skip 'not tested under Windows' if node['platform_family'] == 'windows'
-    packer_dir = "/usr/local/packer-#{node['sbp_packer']['version']}"
-    directory(packer_dir)
+    directory(@packer_dir)
       .must_exist
       .with(:mode, '755')
       .and(:owner, 'root')
       .and(:group, 'root')
     link('/usr/local/packer')
       .must_exist
-      .with(:to, packer_dir)
+      .with(:to, @packer_dir)
     link('/usr/local/bin/packer')
       .must_exist
-      .with(:to, File.join(packer_dir, 'packer'))
-    file(File.join(packer_dir, 'packer'))
+      .with(:to, File.join(@packer_dir, 'packer'))
+    file(File.join(@packer_dir, 'packer'))
       .must_exist
       .with(:mode, '755')
   end
