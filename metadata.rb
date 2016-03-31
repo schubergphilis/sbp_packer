f4edf0a82f970bc77fef43087e799ac0b2e21efa
Andrew Repton <arepton@schubergphilis.com>
2016-03-31 18:00:35 +0200

    Version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'arepton@schubergphilis.com'
 license 'Apache 2.0'
 description 'Installs/Configures packer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.4.0'
+version '1.4.1'
 
 depends 'ark', '~> 1.0.0'
 depends 'golang', '~> 1.7.0'
