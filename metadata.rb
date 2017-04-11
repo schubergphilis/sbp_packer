13842405462bb31cfdb44b785e8e30a447135818
svanharmelen <svanharmelen@schubergphilis.com>
2017-04-11 10:38:26 +0200

    Bump version to v1.4.7

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'arepton@schubergphilis.com'
 license 'Apache 2.0'
 description 'Installs/Configures packer'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '1.4.6'
+version '1.4.7'
 
 depends 'ark', '>= 1.0.0'
 depends 'golang', '>= 1.7.0'
